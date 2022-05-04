8c98e736758d5953c49a45bbfd2c8fc383b36b42
Mateus Seixas <mateuseixas@icloud.com>
2022-05-04 09:04:17 -0300

    feat: nlw day 03 - Create Feedback Route created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,20 @@
 import express from 'express';
 
+import { prisma } from './prisma';
+
 const server = express();
+
+server.use(express.json());
+
+server.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: { type, comment, screenshot },
+  });
+
+  return res.status(201).json({ data: feedback });
+});
 
 server.listen(3333, () => {
   console.log('Server is now running');
