e07baae7bbdfb8d3055800a6fcfbed096975a7c4
Mateus Seixas <mateuseixas@icloud.com>
2022-05-05 09:36:36 -0300

    feat: nlw day 04 - Snippets and component creation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './src/theme';
+
+import { Widget } from './src/components/Widget';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+
+      <Widget />
+
+      <StatusBar
+        style="light"
+        backgroundColor="transparent"
+        translucent />
     </View>
   );
 }
