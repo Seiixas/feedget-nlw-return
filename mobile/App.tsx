c770e3ac855fc8728251d2309bf6d62bfd38a146
Mateus Seixas <mateuseixas@icloud.com>
2022-05-05 10:08:06 -0300

    feat: nlw day 04 - Floating button created

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { theme } from './src/theme';
 
 import AppLoading from 'expo-app-loading';
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
 
+import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
+
+import { View } from 'react-native';
+ 
 import { styles } from './styles';
+import { theme } from '../../theme';
 
 export function Widget() {
   return (
-    <View style={styles.container}>
+    <>
+      <TouchableOpacity
+        style={styles.button} >
 
-    </View>
+        <ChatTeardropDots 
+          size={24}
+          color={theme.colors.text_on_brand_color}
+          weight="bold"/>
+
+      </TouchableOpacity>
+    </>
   );
 }
