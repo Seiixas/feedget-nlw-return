eae96a10e06039c9a526175f64b77a57f4d2e39d
Mateus Seixas <mateuseixas@icloud.com>
2022-05-05 08:03:32 -0300

    feat: nlw day 03 - Implementing errors

--- server/src/shared/infra/http/app.ts
+++ server/src/shared/infra/http/app.ts
@@ -1,15 +1,39 @@
 import 'reflect-metadata';
-import cors from 'cors';
-import express from 'express';
-
-import { routes } from './routes';
-
+import 'express-async-errors';
 import '../../container';
+
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+
+import { AppError } from '../../errors/AppError';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(routes);
+
+app.use(
+  (
+    err: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Response => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
